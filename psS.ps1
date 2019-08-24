f6de0226bd7b43579062b62fe6be05967e7f32e5
bossEzra <44795168+bossEzra@users.noreply.github.com>
2019-08-23 21:46:14 -0700

    add discription for windows

--- psS.ps1
+++ psS.ps1
@@ -1,4 +1,5 @@
 #grab all running services [powershell]
 #save .csv to vagrant shared file
+#windows method
 
 Get-Service | where {$_.Status -eq "Running"} | Export-Csv -path "C:\vagrant\services.csv"
