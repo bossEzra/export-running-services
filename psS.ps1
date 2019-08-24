824d3676d845430825e642eba274cf51e31ad33f
bossEzra <44795168+bossEzra@users.noreply.github.com>
2019-08-24 15:21:14 -0700

    Update psS.ps1
    added cmd instructions

--- psS.ps1
+++ psS.ps1
@@ -2,4 +2,6 @@
 #save .csv to vagrant shared file
 #windows method
 
+#to run in cmd: powershell -file "C:\vagrant\psS.ps1"
+
 Get-Service | where {$_.Status -eq "Running"} | Export-Csv -path "C:\vagrant\services.csv"
