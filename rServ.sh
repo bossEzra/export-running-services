e252ab0354cf3ef7bafda6c9c3d8a17770477b74
bossEzra <44795168+bossEzra@users.noreply.github.com>
2019-08-23 23:47:12 -0700

    Update rServ.sh

--- rServ.sh
+++ rServ.sh
@@ -1,6 +1,6 @@
 #cd to ubuntuVM folder
 #spin up 
-#ssh into box
+#ssh into box / or use gui > terminal
 #cd to vagrant folder
 #run script (./rServ.sh)
 #running services .txt file is now in shard vagrant folder.
