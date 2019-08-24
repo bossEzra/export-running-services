602bd187a8a1bcfc7cd723e783055bdcaf82a28f
bossEzra <44795168+bossEzra@users.noreply.github.com>
2019-08-23 23:44:27 -0700

    update: added walkthrough

--- rServ.sh
+++ rServ.sh
@@ -1,1 +1,8 @@
+#cd to ubuntuVM folder
+#spin up 
+#ssh into box
+#cd to vagrant folder
+#run script (./rServ.sh)
+#running services .txt file is now in shard vagrant folder.
+
 sudo service --status-all 2>&1 | grep + >rServ.txt
