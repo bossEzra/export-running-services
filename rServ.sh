a2b5c011096ea47b84ac37e439ae63cf9614e500
bossEzra <44795168+bossEzra@users.noreply.github.com>
2019-08-24 14:44:56 -0700

    Update rServ.sh

--- rServ.sh
+++ rServ.sh
@@ -2,7 +2,7 @@
 #spin up 
 #ssh into box / or use gui > terminal
 #cd to vagrant folder
-#run script (./rServ.sh)
-#running services .txt file is now in shard vagrant folder.
+#run script (./rServ.sh) <from shared folder>
+#running services .txt file is now inside vagrant folder.
 
 sudo service --status-all 2>&1 | grep + >rServ.txt
